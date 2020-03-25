ebf1512773b1bf378aaf7df0ed77abf69997a29d
Pavel <p.tataurov92@gmail.com>
2020-03-25 17:57:46 +0300

    commit

--- components/products-table/products-table.js
+++ components/products-table/products-table.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import css from './products-table.module.scss'
+import Link from 'next/link'
 
 const product = {
   brandName: 'Sony',
@@ -27,53 +28,59 @@ const ProductsTable = ({ mixin }) => {
           </tr>
         </thead>
         <tbody>
-          {[1, '', -3, '', -2, 4, 1, '', 5, 1].map((changePosition, idx) => {
-            const styles = {
-              changePosition: changePosition < 0 ? ' colorRed' : ''
-            }
-            return (
-              <tr key={idx}>
-                <td>
-                  <div className={css.positionGroup}>
-                    <div className={css.positionWrap}>
-                      <span className={css.position}>{idx + 1}</span>
-                      <span
-                        className={css.changePosition + styles.changePosition}>
-                        {changePosition > 0
-                          ? `+${changePosition}`
-                          : changePosition}
-                      </span>
+          {[1, '', -3, '', -2, 4, 1, '', 5, 1, 1, '', '', '', '', ''].map(
+            (changePosition, idx) => {
+              const styles = {
+                changePosition: changePosition < 0 ? ' colorRed' : ''
+              }
+              return (
+                <tr key={idx}>
+                  <td>
+                    <div className={css.positionGroup}>
+                      <div className={css.positionWrap}>
+                        <span className={css.position}>{idx + 1}</span>
+                        <span
+                          className={
+                            css.changePosition + styles.changePosition
+                          }>
+                          {changePosition > 0
+                            ? `+${changePosition}`
+                            : changePosition}
+                        </span>
+                      </div>
                     </div>
-                  </div>
-                </td>
-                <td>
-                  <div className={css.productImgContainer}>
-                    <img
-                      src={product.img}
-                      alt={`Изображение ${product.name}`}
-                    />
-                  </div>
-                </td>
-                <td className={css.productNameTd}>
-                  <div className={css.productNameContainer}>
-                    <h3 className={css.productName}>{product.name}</h3>
-                  </div>
-                </td>
-                <td>
-                  <span className={css.brandName}>{product.brandName}</span>
-                </td>
-                <td className={css.priceTd}>
-                  <span className={css.price}>{product.price}</span>
-                </td>
-                <td className={css.reviewsTd}>
-                  <span className={css.reviews}>{product.reviews}</span>
-                </td>
-                <td>
-                  <span className={css.rating}>{product.rating}</span>
-                </td>
-              </tr>
-            )
-          })}
+                  </td>
+                  <td>
+                    <div className={css.productImgContainer}>
+                      <img
+                        src={product.img}
+                        alt={`Изображение ${product.name}`}
+                      />
+                    </div>
+                  </td>
+                  <td className={css.productNameTd}>
+                    <Link href='#'>
+                      <a className={css.productName}>{product.name}</a>
+                    </Link>
+                  </td>
+                  <td>
+                    <Link href='#'>
+                      <a className={css.brandName}>{product.brandName}</a>
+                    </Link>
+                  </td>
+                  <td className={css.priceTd}>
+                    <span className={css.price}>{product.price}</span>
+                  </td>
+                  <td className={css.reviewsTd}>
+                    <span className={css.reviews}>{product.reviews}</span>
+                  </td>
+                  <td>
+                    <span className={css.rating}>{product.rating}</span>
+                  </td>
+                </tr>
+              )
+            }
+          )}
         </tbody>
       </table>
     </div>
